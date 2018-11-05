2d505094c41468d9ce4c62c7238d928ed0f0f322
PACHECO Luc <pachecoluc@mbp-de-admin-2.edu.sir>
2018-11-05 13:46:49 +0100

    Updates

---
+++ precache-manifest.da975fef17b9b5df712c23c86e33a36b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "e36cabf1152409faed22",
+    "url": "/blindtest-spotify/static/js/main.e36cabf1.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "e36cabf1152409faed22",
+    "url": "/blindtest-spotify/static/css/main.29ed30b6.chunk.css"
+  },
+  {
+    "revision": "5ca7f2abb83bfaf9e0f63e964838a050",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
