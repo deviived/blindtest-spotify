e53f79f84047072c0464f4d3ba2639f4e1b49fa3
PACHECO Luc <pachecoluc@mbp-de-admin-2.edu.sir>
2018-11-08 11:33:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken = 'BQAWJDFJR5qXWo4szW2gFIjnih8hoHouWnipUcnXKa8GaL6sbEFCNl1oCrNcp-EpthQttGieb7BnTsRuFdESeh84VXEAwR9w_mtSlF7iVQMJjE0c5kQafKQiDNPV15SPo7fCD9A4WuKU8gw7jGBbQDmIlFy6G4nI5hQ';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -28,26 +28,74 @@ function getRandomNumber(x) {
   return Math.floor(Math.random() * x);
 }
 
+class AlbumCover extends Component {
+  render() {
+    const src = this.props.track.album.images[0].url;
+    const alt = 'Album cover for ' + this.props.track.album.name;
+    return (<img src={src} alt={alt} style={{ width: 400, height: 400 }}/>);
+  }
+}
+
 class App extends Component {
 
   constructor() {
     super();
+      this.state = {
+        text: "",
+        tracks: null,
+        tracksLoaded: false,
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://api.spotify.com/v1/me/tracks', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + apiToken,
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          tracksLoaded:true,
+          tracks: data.items,
+        });
+      });
+
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Bienvenue sur le Blindtest</h1>
-        </header>
-        <div className="App-images">
-          <p>Texte modifié</p>
+    if (!this.state.tracksLoaded) {
+      return (
+        <div className="App">
+          <img src={loading} alt="Chargement en cours"/>
         </div>
-        <div className="App-buttons">
+      );
+    }
+    else{
+      const track0 = this.state.tracks[0];
+      const track1 = this.state.tracks[1];
+      const track2 = this.state.tracks[2];
+      return (
+        console.log(this.state.tracks[0].track.name),
+        console.log(track1.track.preview_url),
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1 className="App-title">Bienvenue sur le Blindtest</h1>
+          </header>
+          <div className="App-images">
+          <AlbumCover track = {track1.track} />
+
+          <Sound url={track1.track.preview_url} playStatus={Sound.status.PLAYING}/>
+            <p>Nous avons chargé {this.state.tracks.length} chansons.</p>
+            <p>Titre de la première chanson : {this.state.tracks[0].track.name}.</p>
+          </div>
+          <div className="App-buttons">
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
